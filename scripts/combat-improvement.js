abd8cefbd33dd143a53266dd2b7189d961c98c7e
Fenix20xx <65427807+Fenix20xx@users.noreply.github.com>
2023-02-22 11:26:19 +1300

    Update and rename hello-world.js to combat-improvement.js

---
+++ scripts/combat-improvement.js
@@ -0,0 +1,11 @@
+class CombatImprovement {
+  static Id = 'combat-improvement';
+  
+  static FLAGS = {
+    ROLL: 'roll'
+  }
+  
+  static TEMPLATES = {
+    COMBATIMPROVER: `modules/${this.ID}/templates/combatimprovement.hbs`
+  }
+}
--- scripts/hello-world.js
+++
@@ -1,7 +0,0 @@
-console.log("Running");
-hooks.on("init",function() {
-  console.log("Foundry initialisation begun");
-});
-hooks.on("ready", function() {
-  console.log("Hello World!");
-});
