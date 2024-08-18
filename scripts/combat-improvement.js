0808eb01ae0a67e33ebdfdd41549ca213974425a
Fenix20xx <65427807+Fenix20xx@users.noreply.github.com>
2024-08-18 17:39:48 +1200

    Update combat-improvement.js
    Switching hooks in

--- scripts/combat-improvement.js
+++ scripts/combat-improvement.js
@@ -1,9 +1,11 @@
 class CombatImprovement {
   static Id = 'combat-improvement';
   
-  static FLAGS = {
-    ROLL: 'roll'
-  }
+  Hooks.on("Init": function() {
+    Console.log("Combat Improvement is Running!")
+  });
+
+  Hooks.on
   
   static TEMPLATES = {
     COMBATIMPROVER: `modules/${this.ID}/templates/combatimprovement.hbs`
